9bc26bd10d7e0de30ae599e6fcb77fe5e695cd24
Per-Victor Persson <dracyr@gmail.com>
2014-10-17 12:38:50 +0200

    Changing cap user

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,7 @@
 # config valid only for Capistrano 3.1
 lock '3.2.1'
 set :application, 'pvpersson'
-set :deploy_user, 'rails'
+set :deploy_user, 'root'
 
 set :stages, 'production'
 set :default_stage, "production"
