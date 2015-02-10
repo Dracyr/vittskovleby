18301af495e940ee812150962b333a6a88b3460e
Per-Victor Persson <dracyr@gmail.com>
2015-02-10 21:00:59 +0100

    Added date to posts, styling

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -35,7 +35,7 @@ class PostsController < ApplicationController
   private
 
   def post_params
-    params.require(:post).permit(:title, :content)
+    params.require(:post).permit(:title, :content, :date)
   end
 
 end
--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -2,5 +2,6 @@ class StaticController < ApplicationController
   layout "static_layout"
 
   def home
+    @posts = Post.take(3)
   end
 end
