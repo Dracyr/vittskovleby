b5d37b6391cb3df8d8a0edb8f7d2223ad311cc63
Per-Victor Persson <dracyr@gmail.com>
2015-05-09 16:02:12 +0200

    Fix user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,5 @@
 class User < ActiveRecord::Base
   validates :role, presence: true
-  validates :email, presence: true
-  validates :password, presence: true
-  validates :password_confirmation, presence: true
 
   devise :database_authenticatable, :recoverable, :rememberable, :validatable
 
